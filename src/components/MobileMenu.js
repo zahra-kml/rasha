909df652da89a7fca74f3b423ae7211ecb07dc3e
Zahra Kml <zahra1997kml@gmail.com>
2020-09-05 00:14:02 +0430

    menu !

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -16,8 +16,9 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import Collapse from "@material-ui/core/Collapse";
 import List from "@material-ui/core/List";
+import ListItemText from "@material-ui/core/ListItemText";
 
-const drawerWidth = 280;
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   drawer: {
@@ -28,94 +29,14 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerPaper: {
     width: drawerWidth,
-    backgroundColor: "#000000",
   },
-  List: {
-    backgroundColor: "#000000",
-    color: "#999999",
+  list: {
+    width: "100%",
   },
-  ListItem: {
-    marginTop: "5%",
-    "&:hover": {
-      color: "#ffffffff",
-      "& $ListItemIcon": {
-        color: "#23a267",
-      },
-      "& $ExpandIcon": {
-        color: "#ffffffff",
-      },
-    },
-  },
-  openListItem: {
-    marginTop: "5%",
-    color: "#ffffffff",
-    "& $ListItemIcon": {
-      color: "#23a267",
-    },
-    "& $ExpandIcon": {
-      color: "#ffffffff",
-    },
-  },
-  subListItemText: {
-    fontSize: 12,
+  ItemText: {
     fontFamily: "ir_sans",
-  },
-  ListItemText: {
     fontSize: 13,
-    marginTop: theme.spacing(1),
-    fontFamily: "ir_sans",
-  },
-  RightDash: {
-    fontSize: 16,
-    marginLeft: "5%",
-  },
-  LeftDash: {
-    fontSize: 16,
-    marginRight: "5%",
-  },
-  chosenListItem: {
-    marginTop: "5%",
-    color: "#ffffffff",
-    //borderBottom: "1px solid white",
-    "& $ListItemIcon": {
-      color: "#23a267",
-    },
-    "& $ExpandIcon": {
-      color: "#ffffffff",
-    },
-  },
-  SubListItem: {
-    color: "#999999",
-    "&:hover": {
-      color: "#ffffff",
-      "& $LeftDash": {
-        color: "#ffffff",
-        fontSize: 16,
-      },
-      "& $RightDash": {
-        color: "#ffffff",
-        fontSize: 16,
-      },
-    },
-  },
-  chosenSubListItem: {
-    color: "#ffffff",
-    "& $LeftDash": {
-      color: "#23a267",
-      fontSize: 16,
-    },
-    "& $RightDash": {
-      color: "#23a267",
-      fontSize: 16,
-    },
-  },
-  ListItemIcon: {
-    color: "#999999",
-  },
-  ExpandIcon: {
-    marginLeft: "10%",
-    marginRight: "10%",
-    color: "#999999",
+    flexGrow: 1,
   },
 }));
 
@@ -148,85 +69,20 @@ export default function MobileMenu(props) {
                       keepMounted: true, // Better open performance on mobile.
                     }}
                   >
-                    <List className={classes.List} component="nav">
+                    <List className={classes.list} component="nav">
                       {menuArray.map((item, i) => (
                         <>
-                          <ListItem
-                            button
-                            onClick={
-                              item.subMenu
-                                ? () => {
-                                    let tmp = [...menuArray];
-                                    tmp[i].open = !tmp[i].open;
-                                    setMenuArray(tmp);
-                                  }
-                                : () => {
-                                    userContext.setChosenMenuItemId(item.id);
-                                  }
-                            }
-                            className={
-                              item.open
-                                ? classes.openListItem
-                                : userContext.chosenMenuItemId === item.id
-                                ? classes.chosenListItem
-                                : classes.ListItem
-                            }
-                          >
-                            <ListItemIcon className={classes.ListItemIcon}>
-                              <item.icon fontSize="large" />
+                          <ListItem button onClick={() => {}}>
+                            <ListItemIcon>
+                              <item.icon fontSize={"large"} />
                             </ListItemIcon>
-                            <div className={classes.ListItemText}>
+                            <div className={classes.ItemText}>
                               {languageContext.language === "fa"
                                 ? item.FaName
                                 : item.EnName}
                             </div>
-                            {item.open
-                              ? item.subMenu && (
-                                  <ExpandLess
-                                    className={classes.ExpandIcon}
-                                    fontSize="small"
-                                  />
-                                )
-                              : item.subMenu && (
-                                  <ExpandMore
-                                    className={classes.ExpandIcon}
-                                    fontSize="small"
-                                  />
-                                )}
+                            {item.open ? <ExpandLess /> : <ExpandMore />}
                           </ListItem>
-                          <Collapse in={item.open} timeout="auto" unmountOnExit>
-                            <List component="div" disablePadding>
-                              {item.subMenuItems.map((subItem, index) => (
-                                <>
-                                  <ListItem
-                                    button
-                                    className={
-                                      userContext.chosenMenuItemId ===
-                                        item.id &&
-                                      userContext.chosenSubMenuItemId ===
-                                        subItem.id
-                                        ? classes.chosenSubListItem
-                                        : classes.SubListItem
-                                    }
-                                    onClick={() => {
-                                      userContext.setChosenMenuItemId(item.id);
-                                      userContext.setChosenSubMenuItemId(
-                                        subItem.id
-                                      );
-                                    }}
-                                  >
-                                    <div className={classes.LeftDash}>-</div>
-                                    <div className={classes.subListItemText}>
-                                      {languageContext.language === "fa"
-                                        ? subItem.FaName
-                                        : subItem.EnName}
-                                    </div>
-                                    <div className={classes.RightDash}>-</div>
-                                  </ListItem>
-                                </>
-                              ))}
-                            </List>
-                          </Collapse>
                         </>
                       ))}
                     </List>
